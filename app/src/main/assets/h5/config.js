6bff6a540f589e4c896053dd329a76a5c4c2b3e2
Yuting <1459713776@qq.com>
2026-02-18 11:12:32 +0800

    chore: configure supabase publishable key

--- app/src/main/assets/h5/config.js
+++ app/src/main/assets/h5/config.js
@@ -1,5 +1,5 @@
-﻿window.__SUPABASE_URL__ = "";
-window.__SUPABASE_ANON_KEY__ = "";
+﻿window.__SUPABASE_URL__ = "https://ohtyqihanxzkytklufac.supabase.co";
+window.__SUPABASE_ANON_KEY__ = "sb_publishable_yqZuwRvQbPnXwp4A3deqGg_8De21DX4";
 window.__SUPABASE_BUCKET__ = "resume_uploads";
 window.__SUPABASE_TABLE__ = "direct_applications";
 
@@ -8,3 +8,5 @@ window.__NOTIFY_WEBHOOK_SECRET__ = "";
 window.__OWNER_EMAIL__ = "1459713776@qq.com";
 
 window.__DIRECT_APPLY_API__ = "";
+
+
